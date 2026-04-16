449bd2e9801ec69a8df41a410deac5cf563b1894
Maira Lorraine Domaog <domaog.mairalorraine@gmail.com>
2026-04-16 11:49:41 +0800

    WSAT-M7: Connected React to PHP API

--- personal-cv-react/vite.config.js
+++ personal-cv-react/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/Module-2-Lab---HTML-Structure/",
   plugins: [react()],
+  base: "/Module-2-Lab---HTML-Structure/", // Add this line!
 })
